2e0516b0766fed47261d5aa1a726819c08c6494d
djennis-code <denniscoenen1@gmail.com>
2026-03-07 19:34:53 +0100

    Update Program.cs

--- SPMHD/Program.cs
+++ SPMHD/Program.cs
@@ -1,4 +1,4 @@
-﻿using Raylib_cs;
+using Raylib_cs;
 using System.Numerics;
 
 namespace SPH;
@@ -10,19 +10,20 @@ internal static class Program
 
     public static void InitSPH()
     {
-        simulation.particleCount = 625;
+        simulation.particleCount = 900;
         simulation.particleMass = 1.0f;
         simulation.particleRadius = 5.0f;
         simulation.bounds = windowSize;
 
-        simulation.smoothingRadius = 80.0f;
+        simulation.smoothingRadius = 75.0f;
         simulation.NormaliseKernels();
 
-        simulation.refDensity = 1.5f;
-        simulation.pressureConst = 1.5f; // B = rho * cs^2 / gamma
+        simulation.gamma = 7f;
+        simulation.refDensity = 1.2f;
+        simulation.pressureConst = simulation.refDensity * 6f / simulation.gamma; // B = rho * cs^2 / gamma
         simulation.pressureExt = 1.0f;
 
-        simulation.deltaTime = 0.1f;
+        simulation.deltaTime = 0.05f;
         simulation.damping = 0.5f;
         simulation.viscosity = 0.5f;
 
@@ -42,22 +43,30 @@ internal static class Program
 
             Raylib.ClearBackground(Color.Black);
 
-            simulation.DrawParticles();
             simulation.ComputeDensities();
             simulation.ComputeAccelerations();
             simulation.Integrate();
-
-            //debug
-            //int j = 50;
-            //float rho = simulation.densities[j];
-            //Vector2 vel = simulation.velocities[j];
-            //Console.WriteLine(vel);
+            RenderParticles();
 
             Raylib.DrawFPS(5, 5);
             Raylib.EndDrawing();
         }
 
         Raylib.CloseWindow();
+    }
+
+    public static void RenderParticles()
+    {
+        int count = simulation.particleCount;
+        float radius = simulation.particleRadius;
+        Vector2[] positions = simulation.positions;
+
+        for (uint i = 0; i < count; i++)
+        {
+            Color col = i != 550 ? Color.White : Color.Red;
+            Vector2 pos = positions[i];
+            Raylib.DrawCircle((int)pos.X, (int)pos.Y, radius, col);
+        }
     }
 }
 
